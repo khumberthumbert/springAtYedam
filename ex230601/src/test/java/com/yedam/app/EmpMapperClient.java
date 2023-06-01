09d2fbb1112828223f33e95e5f057fe26d50eb4b
김욱환 <lawyeruh@naver.com>
2023-06-01 17:52:25 +0900

    17:52

---
+++ ex230601/src/test/java/com/yedam/app/EmpMapperClient.java
@@ -0,0 +1,25 @@
+package com.yedam.app;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.yedam.app.emp.mapper.EmpMapper;
+import com.yedam.app.emp.service.EmpVO;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "file:src/main/webapp/WEB-INF/spring/root-context.xml")
+public class EmpMapperClient {
+	@Autowired
+	EmpMapper empMapper;
+	
+	@Test
+	public void getEmpInfo() {
+		EmpVO findEmp = empMapper.selectOne(100);
+		assertEquals(findEmp.getFirst_name(), "Steven");
+	}
+}
